02fbf65d68f3d17e20ad0d5ef08b781cd5e25697
Marcin Sola <marcinsolaonline@gmail.com>
2021-07-15 21:25:07 +0200

    All tasks done - revision 1

---
+++ app/README.md
@@ -0,0 +1,9 @@
+"dependencies": {
+    "openzeppelin-solidity": "^4.2.0",
+    "truffle-hdwallet-provider": "^1.0.17",
+    "web3": "^1.3.5"
+  }
+
+  Token name: SolskiToken
+  Token symbol: SOLA
+  Token address: 0x49739F6781daD3D166759aE61Da5eB69EA99CfE8
--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -13,8 +13,8 @@ contract StarNotary is ERC721 {
     // Implement Task 1 Add a name and symbol properties
     // name: Is a short name to your token
     // symbol: Is a short string like 'USD' -> 'American Dollar'
-    string private name_ = "Star Notary";
-    string private symbol_ = "STR";
+    string private name_ = "SolskiToken";
+    string private symbol_ = "SOLA";
 
     // mapping the Star with the Owner Address
     mapping(uint256 => Star) public tokenIdToStarInfo;
